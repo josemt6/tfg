4163297434aae28f04dee9249796584b2430c373
josemt6 <123760988+josemt6@users.noreply.github.com>
2023-05-08 18:14:27 +0200

    Carpeta de php

--- Pagina/js/misSript.js
+++ Pagina/js/misSript.js
@@ -1,4 +1,22 @@
-
 $(document).ready(function(){
-   
+    $("#btnSesion").click(function(){
+        let usuario = $("#inpUsuario").val()
+        let clave = $("#inpClave").val()
+        $.ajax({
+            url: "../php/usuario.php",
+            data: {
+                "usuario" : usuario,
+                "clave" : clave
+            },
+            type: "GET",
+            success: function(datos){
+                if (datos = "error") {
+                    
+                } else {
+                    
+                }
+            },
+        })
+    })
 })
+
