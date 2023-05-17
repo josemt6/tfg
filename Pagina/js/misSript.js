2ba99e6467e0206de5983e3e2869039e457cbed1
josemt6 <123760988+josemt6@users.noreply.github.com>
2023-05-17 19:31:57 +0200

    Login 2

--- Pagina/js/misSript.js
+++ Pagina/js/misSript.js
@@ -1,34 +1,63 @@
-$(document).ready(function(){
 
-    alert(localStorage.getItem("usuario"))
+function contenidoCanvasSesion() {
+    return "<div>" +
+        "<p><label for='inpUsuario'>Usuario</label></p>" +
+        "<input type='text' name='usuario' id='inpUsuario' class='form-control'>" +
+        "<p><label for='inpClave'>Clave</label></p>" +
+        "<input type='password' name='inpClave' id='inpClave' class='form-control'>" +
+        "<a href='#' class='btn btn-sesion my-4' id='btnSesion'>Iniciar sesión</a>" +
+        "<p class='fw-bold fs-6'>¿Aún no tienes cuenta? , regístrate <a href='#' class='text-azul'>aquí</a></p>" +
+        "<div id='errorSesion' class='text-red'></div>" +
+        "</div>"
+}
+
+function contenidoCanvasSesionIniciada() {
+    return "<div>" +
+        "<div class='col-3 rounded-circle bg-negroFooter text-white' id='letraIcono'></div>" +
+        "<div id='contenidoTxtCanvas' class='col-9 fw-bold text-negroFooter'></div>" +
+        "</div>"
+}
+
+
+$(document).ready(function () {
+
+    if (localStorage.getItem('usuario') != undefined || localStorage.getItem('usuario') != "") {
+        $('#sesion').html(localStorage.getItem('usuario'))
+        $("#contenidoCanvas").append(contenidoCanvasSesion())
+        $("#contenidoTxtCanvas").text(`Hola ${localStorage.getItem("usuario")}, bienvenido a R-tracker`)
+
+    } else {
+        $('#sesion').text('Iniciar sesión')
+        $("contenidoCanvas").append(contenidoCanvasSesion())
+        $('#btnSesion').click(function () {
+            let usuario = $('#inpUsuario').val()
+            alert(usuario)
+            let clave = $('#inpClave').val()
+            $.ajax({
+                url: '../php/usuario.php',
+                data: {
+                    'usuario': usuario,
+                    'clave': clave
+                },
+                type: 'GET',
+                success: function (datos) {
+                    if (datos == false) {
+                        $('#errorSesion').text('No se ha encontrado el usuario o la contraseña')
+                    } else {
+                        localStorage.setItem('usuario', JSON.stringify(usuario))
+                    }
+                },
+                error: function (e) {
+                    alert('Error al cargar el usuario y la contraseña')
+                    console.log(e)
+                }
+            })
+        })
+    }
 
     //Iniciar sesión
 
-    $("#btnSesion").click(function(){
-        let usuario = $("#inpUsuario").val()
-        alert(usuario)
-        let clave = $("#inpClave").val()
-        $.ajax({
-            url: "../php/usuario.php",
-            data: {
-                "usuario" : usuario,
-                "clave" : clave
-            },
-            type: "GET",
-            success: function(datos){
-                if (datos == false) {
-                    $("#errorSesion").text("No se ha encontrado el usuario o la contraseña")
-                } else {
-                    localStorage.setItem("usuario" , usuario)
-                    alert(localStorage.getItem("usuario"))
-                }
-            },
-            error: function(e){
-                alert("Error al cargar el usuario y la contraseña")
-                console.log(e)
-            }
-        })
-    })
+
 
     //Registrarse
 
